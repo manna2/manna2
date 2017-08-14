4407effa9916d9a97a120a9f6221f2a9f09d7b1c
김혜민 <hyemin137@gmail.com>
2017-08-14 22:01:56 +0900

    meeting fragment 에 미팅목록 추가

---
+++ app/src/main/java/org/ssutown/manna2/MeetingRoom/Add_Meeting.java
@@ -0,0 +1,69 @@
+package org.ssutown.manna2.MeetingRoom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.ssutown.manna2.R;
+
+import java.util.ArrayList;
+
+public class Add_Meeting extends AppCompatActivity {
+
+//    userInfo userInfo;
+
+    long meetingRoomID;
+    meeting_Info meeting_info;
+    ArrayList<String> meetinglist = new ArrayList<>();
+
+    EditText meetingName;
+    DatePicker startDay;
+    DatePicker endDay;
+    Button addMeeting;
+    EditText min;
+
+    long userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__meeting);
+
+        meetingName = (EditText) findViewById(R.id.meeting_name);
+        startDay = (DatePicker)findViewById(R.id.startDay);
+        endDay = (DatePicker)findViewById(R.id.endDay);
+        addMeeting = (Button)findViewById(R.id.add);
+        min = (EditText)findViewById(R.id.min);
+
+        Intent i = getIntent();
+        userID = i.getExtras().getLong("user_id");
+
+        addMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                meeting_info = new meeting_Info(meetingName.getText().toString(),"0",startDay.getYear(),startDay.getMonth(),startDay.getDayOfMonth(),endDay.getYear(),endDay.getMonth(),endDay.getDayOfMonth(),min.getText().toString());
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+                meeting meeting = new meeting(meeting_info.getMeeting_id());
+                databaseReference.child("user_Info").child(String.valueOf(userID)).child("personalMeetingList").push().setValue(meeting);
+
+                String key = databaseReference.child("meeting_List").push().getKey();
+                databaseReference.child("meeting_List").child(key).setValue(meeting_info);
+
+                User user = new User(String.valueOf(userID));
+                databaseReference.child("meeting_Info").child(meeting_info.getMeeting_id()).child("Users").push().setValue(user);
+
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/ssutown/manna2/MeetingRoom/meeting_Info.java
@@ -0,0 +1,126 @@
+package org.ssutown.manna2.MeetingRoom;
+
+/**
+ * Created by HyeMin on 2017. 5. 17..
+ */
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class meeting_Info implements Serializable {
+
+    private String meeting_name;
+    private String meeting_id;
+
+    private int startYear;
+    private int startMonth;
+    private int startDay;
+
+    private int endYear;
+    private int endMonth;
+    private int endDay;
+    private String min;
+
+    public meeting_Info(){};
+
+    public meeting_Info(String name, String id, int sY, int sM, int sD, int eY, int eM, int eD, String min){
+
+        Random random = new Random();
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
+        Date date = new Date();
+        String today = df.format(date);
+        String ran = String.valueOf(random.nextInt()%9000+10000);
+
+        meeting_id = today+ran;
+        meeting_name = name;
+        startYear = sY;
+        startMonth = sM;
+        startDay = sD;
+        endYear = eY;
+        endMonth = eM;
+        endDay = eD;
+        min = min;
+
+    }
+
+    public String getMeeting_id() {
+        return meeting_id;
+    }
+
+    public String getMeeting_name() {return meeting_name;}
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public int getStartMonth() {
+        return startMonth;
+    }
+
+    public int getStartDay(){
+        return startDay;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public int getEndMonth() {
+        return endMonth;
+    }
+
+    public int getEndDay(){
+        return endDay;
+    }
+
+    public String getMin() {
+        return min;
+    }
+
+    public void setStartYear(int startYear) {
+        this.startYear = startYear;
+    }
+
+    public void setStartMonth(int startMonth) {
+        this.startMonth = startMonth;
+    }
+
+    public void setStartDay(int startDay) {
+        this.startDay = startDay;
+    }
+
+    public void setEndYear(int endYear) {
+        this.endYear = endYear;
+    }
+
+    public void setEndMonth(int endMonth) {
+        this.endMonth = endMonth;
+    }
+
+    public void setEndDay(int endDay) {
+        this.endDay = endDay;
+    }
+
+    public void setMeeting_id(String meeting_id) {
+        this.meeting_id = meeting_id;
+    }
+
+    public void setMeeting_name(String meeting_name) {
+        this.meeting_name = meeting_name;
+    }
+
+    public void setMin(String min) {
+        this.min = min;
+    }
+
+    /*    public String getFullStartDay(){
+        return String.valueOf(startYear)+String.valueOf(startMonth)+String.valueOf(startDay);
+    }
+
+    public String getFullEndDay(){
+        return String.valueOf(endYear)+String.valueOf(endMonth)+String.valueOf(endDay);
+    }
+*/
+}
