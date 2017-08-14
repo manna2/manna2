938364eb0c0e2021b568a36a10b91869d62b946a
Jiyeon <96_08_01@naver.com>
2017-08-14 15:25:06 +0900

    listview추가염 ~

--- app/src/main/java/org/ssutown/manna2/Fragment/FragmentMeeting.java
+++ app/src/main/java/org/ssutown/manna2/Fragment/FragmentMeeting.java
@@ -1,18 +1,22 @@
 package org.ssutown.manna2.Fragment;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import org.ssutown.manna2.MeetingListview.ListViewAdapter;
 import org.ssutown.manna2.R;
+import org.ssutown.manna2.MeetingListview.ListViewItem;
 
 
 public class FragmentMeeting extends Fragment {
-    TextView textView;
 
     public static FragmentMeeting newInstance(String text){
         FragmentMeeting fragmentMeeting=new FragmentMeeting();
@@ -25,8 +29,46 @@ public class FragmentMeeting extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_meeting,container,false);
-        textView= (TextView) view.findViewById(R.id.textView);
-        textView.setText(getArguments().getString("text"));
+
+        ListViewAdapter adapter;
+
+        adapter = new ListViewAdapter();
+        final ListView listview = (ListView)view.findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.meeting3),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.meeting3),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.meeting3),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.meeting3),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.meeting3),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position) ;
+
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+                Drawable iconDrawable = item.getIcon() ;
+
+                // TODO : use item data.
+            }
+        }) ;
+
+
+
         return view;
     }
+
+
 }
