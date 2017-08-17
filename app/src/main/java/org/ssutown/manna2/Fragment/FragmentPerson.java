ebeaaa5231fef1363dd1119d4b7a0e267b9d3237
Jiyeon <96_08_01@naver.com>
2017-08-17 11:11:30 +0900

    내부저장소 ~~~

--- app/src/main/java/org/ssutown/manna2/Fragment/FragmentPerson.java
+++ app/src/main/java/org/ssutown/manna2/Fragment/FragmentPerson.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -33,9 +34,11 @@ public class FragmentPerson extends Fragment {
         }
 
         Context context = this.getActivity();
+        final SharedPreferences login = context.getSharedPreferences("login", Context.MODE_PRIVATE);
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-        // 제목셋팅
+        if(login.getBoolean("key", true)) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            // 제목셋팅
             alertDialogBuilder.setTitle("계정 연동");
 
             // AlertDialog 셋팅
@@ -45,30 +48,38 @@ public class FragmentPerson extends Fragment {
                     .setCancelable(false)
                     .setPositiveButton("구글",
                             new DialogInterface.OnClickListener() {
-                                    public void onClick(
-                                            DialogInterface dialog, int id) {// 프로그램을 종료한다
+                                public void onClick(
+                                        DialogInterface dialog, int id) {// 프로그램을 종료한다
 //                                        Intent intent = new Intent(getActivity(), OutlookTest.class);
 //                                        startActivity(intent);
-                                            //여기에 나헤언니가 작업한 구글 로그인 창 띄우면 됨
-                                  }
-                                })
-                     .setNegativeButton("아웃룩",
-                             new DialogInterface.OnClickListener() {
-                                    public void onClick(
-                                            DialogInterface dialog, int id) {
-                                     // 다이얼로그를 취소한다
-                                        Intent intent = new Intent(getActivity(), OutlookLogin.class);
-                                        startActivity(intent);
+                                    //여기에 나헤언니가 작업한 구글 로그인 창 띄우면 됨
+                                    SharedPreferences.Editor et1 = login.edit();
+                                    et1.putBoolean("key", false);
+                                    et1.commit();
 
-                                 }
-                                });
+                                }
+                            })
+                    .setNegativeButton("아웃룩",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        DialogInterface dialog, int id) {
 
-        // 다이얼로그 생성
-        AlertDialog alertDialog = alertDialogBuilder.create();
+                                    SharedPreferences.Editor et1 = login.edit();
+                                    et1.putBoolean("key", false);
+                                    et1.commit();
 
-        // 다이얼로그 보여주기
-        alertDialog.show();
+                                    Intent intent = new Intent(getActivity(), OutlookLogin.class);
+                                    startActivity(intent);
 
+                                }
+                            });
+
+            // 다이얼로그 생성
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // 다이얼로그 보여주기
+            alertDialog.show();
+        }
 
         return view;
     }
