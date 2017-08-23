977d0922971d31561662daf02a32d7a70e4a3dca
Jiyeon <96_08_01@naver.com>
2017-08-23 17:12:43 +0900

    meeting member id 불러오기

--- app/src/main/java/org/ssutown/manna2/MeetingFragment/MeetingMainActivity.java
+++ app/src/main/java/org/ssutown/manna2/MeetingFragment/MeetingMainActivity.java
@@ -4,11 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.ssutown.manna2.R;
+
+import java.util.ArrayList;
 
 public class MeetingMainActivity extends FragmentActivity {
     public static String meetingid;
     FragmentSample fragmentSample;
+    public static ArrayList<String> MemberID;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,23 @@ public class MeetingMainActivity extends FragmentActivity {
 
         Intent i = getIntent();
         meetingid = i.getExtras().getString("meetingid");
+
+        MemberID = new ArrayList<String>();
+        databaseReference.child("meeting_Info").child(meetingid).child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                MemberID.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    MemberID.add(ds.getValue().toString());
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
     }
 
 
