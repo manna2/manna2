4e9700c7cab78f14e43ece40375b1f97456fe54d
Jiyeon <96_08_01@naver.com>
2017-08-24 13:41:22 +0900

    Meeting Info String으로 바꾸기

--- app/src/main/java/org/ssutown/manna2/MeetingFragment/MeetingMainActivity.java
+++ app/src/main/java/org/ssutown/manna2/MeetingFragment/MeetingMainActivity.java
@@ -3,6 +3,7 @@ package org.ssutown.manna2.MeetingFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,9 +11,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.ssutown.manna2.MeetingRoom.User;
+import org.ssutown.manna2.MeetingRoom.meeting_Info;
 import org.ssutown.manna2.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MeetingMainActivity extends FragmentActivity {
     public static String meetingid;
@@ -20,6 +25,7 @@ public class MeetingMainActivity extends FragmentActivity {
     public static ArrayList<String> MemberID;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = database.getReference();
+    public static String startendDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +43,7 @@ public class MeetingMainActivity extends FragmentActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 MemberID.clear();
                 for(DataSnapshot ds: dataSnapshot.getChildren()) {
-                    MemberID.add(ds.getValue().toString());
+                    MemberID.add(ds.getValue(User.class).getUserID());
                 }
             }
             @Override
@@ -47,7 +53,53 @@ public class MeetingMainActivity extends FragmentActivity {
 
         });
 
+        databaseReference.child("meeting_List").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+                    Log.i("startendday6", startendDate+"111");
+                   if(meetingid.equals(ds.getValue(meeting_Info.class).getMeeting_id())){
+                       Log.i("startendday2", startendDate+"111");
+                       String temp1 = ds.getValue(meeting_Info.class).getStartYear()+
+                               ds.getValue(meeting_Info.class).getStartMonth()+
+                               ds.getValue(meeting_Info.class).getStartDay();
+                       String temp2 = ds.getValue(meeting_Info.class).getEndYear()+
+                               ds.getValue(meeting_Info.class).getEndMonth()+
+                               ds.getValue(meeting_Info.class).getEndDay();
+                       try{
+                           startendDate = temp1+ diffOfDate(temp1, temp2);
+                           Log.i("startendday3", startendDate+"111");
+
+                       }
+                       catch(java.lang.Exception e){
+                       }
+
+                   }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+        Log.i("startendday4", startendDate+"111");
+
     }
+    public static String diffOfDate(String begin, String end) throws Exception
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+
+        Date beginDate = formatter.parse(begin);
+        Date endDate = formatter.parse(end);
+
+        long diff = endDate.getTime() - beginDate.getTime();
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+        String result = String.valueOf(diffDays);
+        return result;
+    }
+
+
 
 
 }
