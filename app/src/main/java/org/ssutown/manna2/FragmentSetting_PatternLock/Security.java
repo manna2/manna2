d880db378deadc08146f110774f31c11867d963b
윤나혜 <pqlove0256@gmail.com>
2017-08-30 14:35:12 +0900

    암호잠금 완성

--- app/src/main/java/org/ssutown/manna2/FragmentSetting_PatternLock/Security.java
+++ app/src/main/java/org/ssutown/manna2/FragmentSetting_PatternLock/Security.java
@@ -1,7 +1,9 @@
 package org.ssutown.manna2.FragmentSetting_PatternLock;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -18,7 +20,10 @@ public class Security extends Activity {
         setContentView(R.layout.activity_security);
         CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox);
 
-        checkbox.setChecked(false); //우선 초기에 checkbox는 false로 설
+//        Log.v("lllllll",String.valueOf(getPreferences()));
+
+        checkbox.setChecked(getPreferences());
+
 
         checkbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
@@ -29,17 +34,30 @@ public class Security extends Activity {
                     if (isChecked) {
 //                        Toast.makeText(getApplicationContext(), "눌림", Toast.LENGTH_SHORT).show();
 
-                        Intent intent = new Intent(getApplicationContext(),PatternLock.class);
-                        startActivity(intent);
+                        if(getPreferences() == false)
+                        {
+                            Intent intent = new Intent(getApplicationContext(),PatternLock.class);
+                            startActivity(intent);
+
+                            finish();
+                        }
+                        else if(getPreferences() == true)
+                        {
+
+                        }
 
                     } else {
 //                            Toast.makeText(getApplicationContext(), "안눌림", Toast.LENGTH_SHORT).show();
-
 
                     }
                 }
             }
         });
+    }
+
+    private Boolean getPreferences(){
+        SharedPreferences pref = getSharedPreferences("prefC", MODE_PRIVATE);
+        return pref.getBoolean("on/off",false);
     }
 }
 
